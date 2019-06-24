b3ec489d506b092a4ca2a8cf25d998aeffc7cc53
kotasho <sho.progress91@gmail.com>
2019-06-24 15:28:35 +0900

    Add-top-page-budget

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,12 +2,37 @@ class BooksController < ApplicationController
 
 def index
   @name = current_user.name
+  # 今月は何月か
+  time = Time.now
+  this_month = time.month
+  this_year = time.year
+  
+  # 設定した予算の今月
+  @income_budget = Budget.find_by(year: this_year, month: this_month).income_amount
+  @spending_budget = Budget.find_by(year: this_year, month: this_month).spending_amount
+  
+  # 今月の現在の収支状況
+  
+  @current_income = Income.group("YEAR(date)").group("MONTH(date)").sum(:amount)
+  @current_income[[this_year,this_month]]
+  @current_income_view = @current_income[[this_year,this_month]]
+  
+  @current_spending = Spending.group("YEAR(date)").group("MONTH(date)").sum(:amount)
+  @current_spending[[this_year,this_month]]
+  @current_spending_view = @current_spending[[this_year,this_month]]
+
+  # binding.pry
 end
 
 def new
 end
 
 def show
+  time = Time.now
+  @today = Income
+  # @income = Budget.find(params[:id])
+  
+  # binding.pry
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'books#index'
-  get 'books/show' => 'books#show'
+  get 'books/:id/' => 'books#show'
   resources :users, only: [:index, :new, :create]
-  get 'users/:id'  => 'users#show'
+  get 'users/:id/'  => 'users#show'
   get 'users/:id/new' => 'users#new'
   post 'users/:id' => 'users#create'
   
