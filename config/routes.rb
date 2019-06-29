b03fafc41a091fbc5b492606403a73caf12afc80
kotasho <sho.progress91@gmail.com>
2019-06-29 13:54:57 +0900

    Add-today-detail-edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'books#index'
   get 'books/:id/' => 'books#show'
+  get 'books/:id/edit' => 'books#edit'
+  patch 'books/:id' => 'books#update'
   resources :users, only: [:index, :new, :create]
   get 'users/:id/'  => 'users#index'
   post 'users/:id(/:name)' => 'users#show'
   get 'users/:id/new' => 'users#new'
   get 'users/:id/'  => 'users#show'
+  get 'users/:id/edit' => 'users#edit'
   post 'users/:id' => 'users#create'
+ 
   
   
   
